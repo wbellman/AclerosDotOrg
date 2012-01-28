125afc5bbc8c99d6616f207cc8fc72010a754330
Will Bellman <wbellman@gmail.com>
2012-01-28 13:13:49 -0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AclerosOrg::Application.config.secret_token = '7b0a7d217c747eb2646ab6ecb88395a4aa99f5cc8ea64601863a62c0cc8fc41d2b6db18fb0eaeaca09177c35f59505fb43c15607f00b440e5f771e39f9af0981'
